0b0ddbfa908930753c75a1dde4e12e04c5d587e2
lmihalkovic <laurentm@wherever.io>
2017-12-19 13:33:52 +0100

    missing info

--- Package.swift
+++ Package.swift
@@ -1,13 +1,25 @@
 // swift-tools-version:4.0
 import PackageDescription
 
+//#if os(Linux) || os(macOS) || os(iOS) 
+
 let package = Package(
     name: "SGLFreeType",
     pkgConfig: "freetype2",
     providers: [
-      .brew(["freetype2"]), 
+      .brew(["freetype"]), 
       .apt(["libfreetype6-dev"])
-    ]
-
+    ],
+    products: [
+      .library(name: "SGLFreeType", targets: ["SGLFreeType"])
+    ],
+    dependencies: [],
+    targets: [
+      .target(name: "SGLFreeType", dependencies: [])
+    ]    
 )
+
+//#else
+//fatalError("Unsupported OS")
+//#endif
 
